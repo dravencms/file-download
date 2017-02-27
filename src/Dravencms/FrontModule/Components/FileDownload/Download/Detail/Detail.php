468833302bced0a45c7a8c3075b016a3546e1be7
Adam Schubert <adam.schubert@sg1-game.net>
2017-02-27 07:48:29 +0100

    Fix frontend

--- src/Dravencms/FrontModule/Components/FileDownload/Download/Detail/Detail.php
+++ src/Dravencms/FrontModule/Components/FileDownload/Download/Detail/Detail.php
@@ -3,8 +3,12 @@
 namespace Dravencms\FrontModule\Components\FileDownload\Download\Detail;
 
 use Dravencms\Components\BaseControl\BaseControl;
+use Dravencms\Locale\CurrentLocale;
+use Dravencms\Model\FileDownload\Entities\DownloadFileTranslation;
 use Dravencms\Model\FileDownload\Repository\DownloadFileRepository;
+use Dravencms\Model\FileDownload\Repository\DownloadFileTranslationRepository;
 use Dravencms\Model\FileDownload\Repository\DownloadRepository;
+use Dravencms\Model\FileDownload\Repository\DownloadTranslationRepository;
 use IPub\VisualPaginator\Components\Control;
 use Salamek\Cms\ICmsActionOption;
 use Salamek\Files\FileStorage;
@@ -17,19 +21,39 @@ class Detail extends BaseControl
     /** @var DownloadRepository */
     private $downloadRepository;
 
+    /** @var DownloadTranslationRepository */
+    private $downloadTranslationRepository;
+
+    /** @var DownloadFileTranslationRepository */
+    private $downloadFileTranslationRepository;
+
     /** @var DownloadFileRepository */
     private $downloadFileRepository;
+
+    /** @var CurrentLocale */
+    private $currentLocale;
 
     /** @var FileStorage */
     private $fileStorage;
 
-    public function __construct(ICmsActionOption $cmsActionOption, DownloadRepository $downloadRepository, DownloadFileRepository $downloadFileRepository, FileStorage $fileStorage)
+    public function __construct(
+        ICmsActionOption $cmsActionOption,
+        DownloadRepository $downloadRepository,
+        DownloadTranslationRepository $downloadTranslationRepository,
+        DownloadFileRepository $downloadFileRepository,
+        DownloadFileTranslationRepository $downloadFileTranslationRepository,
+        CurrentLocale $currentLocale,
+        FileStorage $fileStorage
+    )
     {
         parent::__construct();
         $this->cmsActionOption = $cmsActionOption;
         $this->downloadRepository = $downloadRepository;
         $this->downloadFileRepository = $downloadFileRepository;
+        $this->downloadTranslationRepository = $downloadTranslationRepository;
+        $this->downloadFileTranslationRepository = $downloadFileTranslationRepository;
         $this->fileStorage = $fileStorage;
+        $this->currentLocale = $currentLocale;
     }
 
 
@@ -38,15 +62,21 @@ class Detail extends BaseControl
         $template = $this->template;
         $download = $this->downloadRepository->getOneById($this->cmsActionOption->getParameter('id'));
 
-
         $visualPaginator = $this['visualPaginator'];
 
         $paginator = $visualPaginator->getPaginator();
         $paginator->itemsPerPage = 10;
         $paginator->itemCount = $download->getDownloadFiles()->count();
 
-        $template->download = $download;
-        $template->files = $this->downloadFileRepository->getByDownload($download, $paginator->itemsPerPage, $paginator->offset);
+        $template->downloadTranslation = $this->downloadTranslationRepository->getTranslation($download, $this->currentLocale);
+
+        $fileTranslations = [];
+        foreach ($this->downloadFileRepository->getByDownload($download, $paginator->itemsPerPage, $paginator->offset) AS $item)
+        {
+            $fileTranslations[] = $this->downloadFileTranslationRepository->getTranslation($item, $this->currentLocale);
+        }
+        
+        $template->fileTranslations = $fileTranslations;
 
         $template->setFile(__DIR__.'/detail.latte');
         $template->render();
